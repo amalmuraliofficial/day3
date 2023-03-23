b5bee1959916ac255d7b913b2cfe4cc729f53343
Amal Murali <amalmurali1100@gmail.com>
2023-03-23 19:05:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LabelComponent } from './label/label.component';
+import { PoolComponent } from './pool/pool.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LabelComponent,
+    PoolComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pool/pool.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pool',
+  templateUrl: './pool.component.html',
+  styleUrls: ['./pool.component.css']
+})
+export class PoolComponent {
+
+  database =[
+    {
+    "id":"1",
+    "title":"Alappuzha",
+    "image":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fe%2Fe4%2FAlappuzha_Boat_Beauty_W.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FAlappuzha&tbnid=Ta2AQzXgGGjhZM&vet=12ahUKEwiP0N76y4f9AhVgHbcAHaKVDwYQMygAegUIARDkAQ..i&docid=0ZLTrNmJNursXM&w=2500&h=1656&q=alappuzha&ved=2ahUKEwiP0N76y4f9AhVgHbcAHaKVDwYQMygAegUIARDkAQ"
+    },
+    {
+      "id":"2",
+      "title":"Eranakulam",
+      "image":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fe%2Fe4%2FAlappuzha_Boat_Beauty_W.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FAlappuzha&tbnid=Ta2AQzXgGGjhZM&vet=12ahUKEwiP0N76y4f9AhVgHbcAHaKVDwYQMygAegUIARDkAQ..i&docid=0ZLTrNmJNursXM&w=2500&h=1656&q=alappuzha&ved=2ahUKEwiP0N76y4f9AhVgHbcAHaKVDwYQMygAegUIARDkAQ",
+    }
+  ]
+}
